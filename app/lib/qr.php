9fae1ef3db00ea0de810c83565972d1570ea5bc9
kmoran-kinetico <kmorankinetico@gmail.com>
2026-07-03 11:42:21 +0200

    Update qr.php

--- app/lib/qr.php
+++ app/lib/qr.php
@@ -1,24 +1,10 @@
-<?php
-
 function generateQR(string $slug)
 {
     if (!is_dir("../qrs")) {
         mkdir("../qrs", 0777, true);
     }
 
-    $url = "/app/contacto/?slug=" . $slug;
+    file_put_contents("../qrs/test.txt", "funciona");
 
-    $file = "../qrs/$slug.svg";
-
-    if (file_exists($file)) {
-        return;
-    }
-
-    $api = "https://api.qrserver.com/v1/create-qr-code/?format=svg&size=600x600&data=" . urlencode($url);
-
-    $svg = @file_get_contents($api);
-
-    if ($svg !== false) {
-        file_put_contents($file, $svg);
-    }
+    return;
 }
